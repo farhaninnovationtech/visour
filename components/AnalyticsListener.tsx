892a614300b96314160778fff735e9d95552498d
Farhan Innovation & Technologies <innovationfarhan@gmail.com>
2026-09-21 09:38:32 +0500

    Update AnalyticsListener.tsx

--- components/AnalyticsListener.tsx
+++ components/AnalyticsListener.tsx
@@ -1,20 +1,18 @@
-"use client";
-
-import { useEffect } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
-
 export default function AnalyticsListener() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    if (typeof window === "undefined" || typeof window.gtag !== "function") {
+    if (typeof window.gtag !== "function") {
       return;
     }
-    const url = searchParams?.toString()
-      ? `${pathname}?${searchParams.toString()}`
-      : pathname;
-    window.gtag("event", "page_view", { page_path: url });
+
+    const query = searchParams?.toString();
+    const pagePath = query ? `${pathname}?${query}` : pathname;
+
+    window.gtag("event", "page_view", {
+      page_path: pagePath,
+    });
   }, [pathname, searchParams]);
 
   return null;
